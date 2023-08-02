ef5f1029439bc98a3f0be25bcf3a4a82092a30e7
MrRedaM <reda.morsli.1@ens.etsmtl.ca>
2023-08-02 02:49:16 -0400

    Switch to Flannel CNI

--- provisions/inits/master.sh
+++ provisions/inits/master.sh
@@ -10,6 +10,8 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+#kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+
+kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
 
 kubeadm token create --print-join-command > /vagrant/k8s_join_command.txt
